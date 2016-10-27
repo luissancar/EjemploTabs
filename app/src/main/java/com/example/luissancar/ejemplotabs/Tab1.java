511aefe26846e57bc0c3d38eb9bda49ff6cf2566
luissancar <luissancar@yahoo.es>
2016-10-27 13:36:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/luissancar/ejemplotabs/Tab1.java
@@ -0,0 +1,47 @@
+package com.example.luissancar.ejemplotabs;
+
+
+
+    import android.os.Bundle;
+    import android.support.v4.app.Fragment;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+
+    /**
+     * Created by luissancar on 25/10/2016.
+     */
+    public class Tab1 extends Fragment {
+
+        @Override
+
+        public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+
+        }
+
+
+
+        @Override
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.tab2, container, false);
+
+        }
+
+
+
+
+
+    public void anadir(View v){
+
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/luissancar/ejemplotabs/Tab4.java
@@ -0,0 +1,33 @@
+package com.example.luissancar.ejemplotabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by luissancar on 27/10/16.
+ */
+
+public class Tab4 extends Fragment {
+
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.tab4, container, false);
+
+    }
+}
